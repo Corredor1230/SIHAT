f7dac6cd34a4e05c29d27b9126e3f8a59e566eb0
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 00:24:27 +0900

    Added TransientResults struct

--- src/results.rs
+++ src/results.rs
@@ -1,7 +1,16 @@
-use crate::structures::BinFrame;
+use crate::structures;
+
+#[derive(Default, Debug)]
+pub struct TransientResults
+{
+    pub range: structures::SampleRange,
+    pub scalogram: Vec<structures::VariableRatePartial>,
+    pub rms: f32
+}
 
 #[derive(Default, Debug)]
 pub struct SihatResults
 {
-    pub o_results: Vec<BinFrame>
+    pub o_results: Vec<structures::BinFrame>,
+    pub t_results: TransientResults
 }
