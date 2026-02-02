0c1f1ab81f9fd0fa925453e493f2513945e2f6f9
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 00:23:45 +0900

    removed audio loading logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-use std::fs::File;
-use std::io::BufReader;
-
-use rodio::{Decoder, Source};
-
 mod analysis;
+mod dsp;
 mod application;
 mod models;
 mod utils;
@@ -11,12 +7,6 @@ mod results;
 mod structures;
 
 fn main(){
-    let file = File::open("src/AGuit1_329_F.wav").unwrap();
-    let source = Decoder::new(BufReader::new(file)).unwrap();
-
-    let channels = source.channels();
-    let sample_rate = source.sample_rate();
-    println!("Channels: {}, Sample Rate: {}", channels, sample_rate);
-
+    
     application::run();
 }
