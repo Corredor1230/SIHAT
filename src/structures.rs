01d594b8176910b10595c39507492dc64739d4bd
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 00:24:48 +0900

    Added SampleRange, VariableRatePartial and SampleValueList

--- src/structures.rs
+++ src/structures.rs
@@ -1,7 +1,29 @@
-#[derive(Default, Debug)]
+#[derive(Clone, Copy, Default, Debug)]
 pub struct BinFrame
 {
     pub freq: f32,
     pub phase: f32,
     pub amp: f32
+}
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct SampleRange
+{
+    pub init: usize,
+    pub end: usize
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct VariableRatePartial
+{
+    pub freq: f32,
+    pub hop_size: usize,
+    pub data: Vec<f32>
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct SampleValueList
+{
+    pub index_list: Vec<usize>,
+    pub value_list: Vec<f32>
 }
