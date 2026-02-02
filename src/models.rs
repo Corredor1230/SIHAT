2757f06bf8c4d84265b7907e2f269f0af5403a5e
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 00:24:08 +0900

    Added TransientSettings model

--- src/models.rs
+++ src/models.rs
@@ -2,29 +2,67 @@ pub struct OvertoneSettings
 {
     pub fft_size: usize,
     pub n_values: usize,
-    pub threshold: f32
+    pub threshold: f32,
+    pub tolerance_in_cents: f32,
+    pub init_sample: usize
 }
 
 impl Default for OvertoneSettings
 {
     fn default() -> Self {
         OvertoneSettings { 
-            fft_size: 2048, 
+            fft_size: 32768, 
             n_values: 32,
-            threshold: -60.0
+            threshold: -60.0,
+            tolerance_in_cents: 100.0,
+            init_sample: 8000
+        }
+    }
+}
+
+pub struct TransientSettings
+{
+    pub rms_sample_size: usize,
+    pub use_ms_size: bool,
+    pub rms_ms_size: f32,
+    pub set_hop_size: bool,
+    pub hop_size: usize,
+    pub start_sample: usize,
+    pub rms_factor: f64,
+    pub rms_threshold: f64,
+    pub correlation_offset: usize
+}
+
+impl Default for TransientSettings
+{
+    fn default() -> Self {
+        TransientSettings { 
+            rms_sample_size: 128,
+            use_ms_size: false,
+            rms_ms_size: 20.0,
+            set_hop_size: false,
+            hop_size: 128,
+            start_sample: 2000,
+            rms_factor: 3.0,
+            rms_threshold: 0.1,
+            correlation_offset: 100
         }
     }
 }
 
 pub struct SihatSettings
 {
-    pub o_settings: OvertoneSettings
+    pub o_settings: OvertoneSettings,
+    pub t_settings: TransientSettings
 }
 
 impl Default for SihatSettings
 {
     fn default() -> Self {
-        SihatSettings { o_settings: Default::default() }
+        SihatSettings { 
+            o_settings: Default::default(),
+            t_settings: Default::default()
+        }
     }
 }
 
