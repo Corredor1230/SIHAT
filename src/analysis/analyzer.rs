7408583e7c6c8cda4ab9a6f898aa8e5b27f41adc
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 00:25:37 +0900

    Passed new overtone results and transient results to analyzer

--- src/analysis/analyzer.rs
+++ src/analysis/analyzer.rs
@@ -10,5 +10,6 @@ use super::transient;
 pub fn analyze(unit: &AudioInfo, sihat_settings: &SihatSettings)
 {
     let mut sihat_results: SihatResults = Default::default();
-    overtone::process(unit, &sihat_settings.o_settings);
+    sihat_results.o_results = overtone::process(unit, &sihat_settings.o_settings);
+    sihat_results.t_results = transient::process(unit, &sihat_settings.t_settings);
 }
