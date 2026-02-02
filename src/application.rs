272ee9d0f108e385c59c3c7c7c1c70385b3b0a3a
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 00:22:07 +0900

    Added normalization factor

--- src/application.rs
+++ src/application.rs
@@ -1,16 +1,20 @@
 use std::fs::File;
+use std::path::Path;
 use std::io::BufReader;
 use rodio::{Decoder, Source};
-use crate::{analysis::analyzer, models::{AudioInfo, SihatSettings}};
+use crate::{analysis::analyzer, models::{AudioInfo, SihatSettings}, utils::normalize_audio};
 
 pub fn run()
 {
     let filename: String = "AGuit1_329_F.wav".to_string();
-    let file = File::open("src/AGuit1_329_F.wav").unwrap();
+    let path = Path::new("src").join(filename.clone());
+    let file = File::open(path).unwrap();
     let source = Decoder::new(BufReader::new(file)).unwrap();
     let channels = source.channels();
     let sample_rate = source.sample_rate();
-    let audio: Vec<f32> = source.collect();
+    let mut audio: Vec<f32> = source.collect();
+    let norm_db: f32 = -1.0;
+    normalize_audio(&mut audio, norm_db);
 
     let audio_info: AudioInfo = AudioInfo { audio_file: audio, sample_rate: sample_rate as f32, file_name: filename };
 
