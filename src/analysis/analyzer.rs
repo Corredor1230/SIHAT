2373710b748934d77a5bd4d31942db3662876be1
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 17:14:07 +0900

    added transient results return function

--- src/analysis/analyzer.rs
+++ src/analysis/analyzer.rs
@@ -9,7 +9,8 @@ use super::transient;
 
 pub fn analyze(unit: &AudioInfo, sihat_settings: &SihatSettings)
 {
+    
     let mut sihat_results: SihatResults = Default::default();
     sihat_results.o_results = overtone::process(unit, &sihat_settings.o_settings);
-    sihat_results.t_results = transient::process(unit, &sihat_settings.t_settings);
+    sihat_results.t_results = transient::process(unit, &sihat_settings.t_settings, unit.meta_pitch);
 }
