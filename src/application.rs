76ec9f8a24d34a843f16cbd58506035db62074f1
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 17:14:19 +0900

    added meta pitch parser

--- src/application.rs
+++ src/application.rs
@@ -8,6 +8,15 @@ pub fn run()
 {
     let filename: String = "AGuit1_329_F.wav".to_string();
     let path = Path::new("src").join(filename.clone());
+    let meta_fields: Vec<&str> = filename.split("_").collect();
+    let meta_pitch: f32 = match meta_fields[1].parse() {
+        Ok(num) => num,
+        Err(e) => {
+            eprintln!("Error parsing float_str: {}", e);
+            // Handle the error, e.g., return a default value or panic
+            0.0
+        }
+    };
     let file = File::open(path).unwrap();
     let source = Decoder::new(BufReader::new(file)).unwrap();
     let channels = source.channels();
@@ -16,7 +25,7 @@ pub fn run()
     let norm_db: f32 = -1.0;
     normalize_audio(&mut audio, norm_db);
 
-    let audio_info: AudioInfo = AudioInfo { audio_file: audio, sample_rate: sample_rate as f32, file_name: filename };
+    let audio_info: AudioInfo = AudioInfo { audio_file: audio, meta_pitch: meta_pitch, sample_rate: sample_rate as f32, file_name: filename };
 
     println!("Channels: {}, Sample Rate: {}", channels, sample_rate);
 
