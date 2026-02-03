ce906c5d68ebfe729626994fe8da308438eb7d7c
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-03 17:13:30 +0900

    started writing cross correlation helper

--- src/dsp/correlation.rs
+++ src/dsp/correlation.rs
@@ -1,11 +1,13 @@
-use crate::structures::SampleValueList;
+use crate::{models::{AudioInfo, CorrelationSettings}, structures::SampleValueList};
 
-pub fn perform_cross_correlation(audio: &Vec<f32>, window_size: usize, start_sample: usize, stop_sample: usize, go_left: bool, jump_post_peak: bool, jump_size: usize) -> SampleValueList
+pub fn perform_cross_correlation(unit: &AudioInfo, pitch: f32, settings: &CorrelationSettings) -> SampleValueList
 {
+    //Initialize
+    let sr = unit.sample_rate;
     let mut index_list: Vec<usize> = Vec::new();
     let mut value_list: Vec<f32> = Vec::new();
-
-
+    let expected_period: usize = (sr / pitch) as usize;
+    
 
     let sample_list: SampleValueList = SampleValueList { index_list, value_list };
     return sample_list;
