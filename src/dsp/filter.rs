bb39fa60583303d5099922f54464340dc783f7bd
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-06 16:55:04 +0900

    Finished filtfilt function for the correlation calculation.

--- src/dsp/filter.rs
+++ src/dsp/filter.rs
@@ -1,10 +1,43 @@
-use crate::models::AudioInfo;
-
-pub fn filtfilt(unit: &AudioInfo, cutoff_freq: f32)
+pub fn filtfilt(input: &mut Vec<f32>, sr: f32, cutoff_freq: f32)
 {
-    assert!(unit.audio_file.is_empty());
-    assert!(unit.audio_file.len() < 2);
+    assert!(!input.is_empty());
+    assert!(input.len() < 2);
 
-    let inner_u = std::f64::consts::PI * cutoff_freq as f64 / unit.sample_rate as f64;
+    let inner_u = std::f64::consts::PI * cutoff_freq as f64 / sr as f64;
+    let u = inner_u.tan();
+    let common = 1.0 + u;
+
+    let b0: f64 = u / common;
+    let b1: f64 = b0;
+    let a1: f64 = (u - 1.0) / common;
+    let mut out: Vec<f64> = input.iter().map(|&x| x as f64).collect();
+    let n = input.len();
+    let mut xPrev = input[0] as f64;
+    let mut yPrev = input[0] as f64;
+
+    for i in 0..n{
+        let xCurr = input[i] as f64;
+        let yCurr = (b0 * xCurr) + (b1 * xPrev) - (a1 * yPrev);
+
+        out[i] = yCurr;
+
+        xPrev = xCurr;
+        yPrev = yCurr;
+    }
+
+    xPrev = out[n - 1];
+    yPrev = out[n - 1];
+
+    for i in (0..(n - 1)).rev(){
+        let xCurr = out[i];
+        let yCurr = (b0* xCurr) + (b1 * xPrev) - (a1 * yPrev);
+
+        out[i] = yCurr;
+
+        xPrev = xCurr;
+        yPrev = yCurr;
+    }
+
+    *input = out.iter().map(|&x| x as f32).collect();
     
 }
