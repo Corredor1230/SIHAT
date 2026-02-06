3de6c7898874ef414a36f72dbd37e3a332c536a2
Corredor1230 <juanpablocorredor97@gmail.com>
2026-02-06 18:57:58 +0900

    Continued developing cross correlation function

--- src/dsp/correlation.rs
+++ src/dsp/correlation.rs
@@ -1,4 +1,4 @@
-use crate::{models::{AudioInfo, CorrelationSettings}, structures::SampleValueList};
+use crate::{models::{AudioInfo, CorrelationSettings}, structures::SampleValueList, utils::{find_peak_sample, find_previous_zero}};
 
 pub fn perform_cross_correlation(unit: &AudioInfo, pitch: f32, settings: &CorrelationSettings) -> SampleValueList
 {
@@ -7,6 +7,14 @@ pub fn perform_cross_correlation(unit: &AudioInfo, pitch: f32, settings: &Correl
     let mut index_list: Vec<usize> = Vec::new();
     let mut value_list: Vec<f32> = Vec::new();
     let expected_period: usize = (sr / pitch) as usize;
+    let sliding_size: usize = (expected_period as f64 * 0.1) as usize;
+    let peak_sample = find_peak_sample(
+        &unit.audio_file, 
+        settings.start_sample, 
+        settings.start_sample + expected_period * 2, 
+        true
+    );
+    let corr_start = find_previous_zero(&unit.audio_file, peak_sample);
     
 
     let sample_list: SampleValueList = SampleValueList { index_list, value_list };
