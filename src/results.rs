216568224810f6ff9993488ac0bebe31fb321f0b
Corredor1230 <juanpablocorredor97@gmail.com>
2026-03-02 00:10:21 +0900

    Added TransientResults and HarmonicResults

--- src/results.rs
+++ src/results.rs
@@ -4,7 +4,18 @@ use crate::structures;
 pub struct TransientResults
 {
     pub range: structures::SampleRange,
+    pub t_length: usize,
     pub scalogram: Vec<structures::VariableRatePartial>,
+    pub scalo_range: structures::SampleRange,
+    pub scalogram_length: usize,
+    pub rms: f32
+}
+
+#[derive(Default, Debug)]
+pub struct HarmonicResults
+{
+    pub h_ratio_envs: Vec<Vec<structures::BinFrame>>,
+    pub index_list: Vec<usize>,
     pub rms: f32
 }
 
@@ -12,5 +23,6 @@ pub struct TransientResults
 pub struct SihatResults
 {
     pub o_results: Vec<structures::BinFrame>,
-    pub t_results: TransientResults
+    pub t_results: TransientResults,
+    pub h_results: HarmonicResults
 }
